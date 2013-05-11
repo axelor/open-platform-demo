149bc0f72200ab2ab87295dbfc65d24a5d1de256
Pierre Belloy <p.belloy@axelor.com>
2013-05-12 00:08:26 +0200

    Fixed calculate service issues.

--- axelor-sale/src/main/groovy/com/axelor/sale/service/SaleOrderService.groovy
+++ axelor-sale/src/main/groovy/com/axelor/sale/service/SaleOrderService.groovy
@@ -1,5 +1,11 @@
 package com.axelor.sale.service
 
+import java.math.RoundingMode;
+
+import javax.validation.ValidationException;
+
+import com.axelor.db.mapper.Adapter;
+import com.axelor.db.mapper.Mapper;
 import com.axelor.rpc.ActionRequest
 import com.axelor.rpc.ActionResponse
 import com.axelor.sale.db.Order
@@ -7,7 +13,9 @@ import com.axelor.sale.db.Order
 class SaleOrderService {
 	
 	void validate(Order order) {
-		assert order?.confirmDate.isAfter(order?.createDate)
+		if (order?.confirmDate?.isBefore(order?.createDate)) {
+			throw new ValidationException("invalid sale order, confirm date is in future")
+		}
 	}
 	
 	Order calculate(Order order) {
@@ -15,17 +23,17 @@ class SaleOrderService {
 		if (!order.items || order.items.empty)
 			return order;
 		
-		def amount = order.items.collect { it.quantity * it.price }.sum()
-		def taxAmount = order.items.collect {
+		BigDecimal amount = order.items.collect { it.quantity * it.price }.sum()
+		BigDecimal taxAmount = order.items.collect {
 			def value = it.quantity * it.price;
 			it.taxes ? it.taxes.collect { value * it.rate }.sum() : 0
 		}.sum()
 
-		def totalAmount = amount + taxAmount
-		
-		order.amount = amount
-		order.taxAmount = taxAmount
-		order.totalAmount = totalAmount
+		BigDecimal totalAmount = amount + taxAmount
+
+		order.amount = amount.setScale(4, RoundingMode.HALF_UP)
+		order.taxAmount = taxAmount.setScale(4, RoundingMode.HALF_UP)
+		order.totalAmount = totalAmount.setScale(2, RoundingMode.HALF_UP)
 		
 		return order
 	}
