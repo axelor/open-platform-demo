e5351ac7a902501c2a90c12c42e8d1c3eb8d047c
Pierre Belloy <p.belloy@axelor.com>
2013-06-10 15:04:53 +0200

    Improved messaging demo.

--- axelor-dyn/src/main/java/com/axelor/dyn/service/DynService.java
+++ axelor-dyn/src/main/java/com/axelor/dyn/service/DynService.java
@@ -1,5 +1,6 @@
 package com.axelor.dyn.service;
 
+import com.axelor.auth.AuthUtils;
 import com.axelor.dyn.db.DynMessage;
 import com.google.inject.persist.Transactional;
 
@@ -9,19 +10,24 @@ public class DynService {
 	public DynMessage sendMessage(DynMessage context, DynMessage reply) {
 
 		if (reply.getSubject() == null) {
-			reply.setSubject("No Subject");
+			reply.setSubject(context.getSubject());
 		}
 		if (context.getRecipients() != null) {
 			reply.setRecipients(context.getRecipients());
 		}
+		if (reply.getAuthor() == null) {
+			reply.setAuthor(AuthUtils.getUser());
+		}
 
-		DynMessage message = reply;
 		if (context.getId() != null) {
-			message = DynMessage.find(context.getId());
-			if (reply != null) {
-				message.addReply(reply);
+			DynMessage parent = DynMessage.find(context.getId());
+			if (parent.getParent() != null) {
+				parent = parent.getParent();
+			}
+			if (parent != null) {
+				reply.setParent(parent);
 			}
 		}
-		return message.save();
+		return reply.save();
 	}
 }
--- axelor-dyn/src/main/java/com/axelor/dyn/web/DynController.java
+++ axelor-dyn/src/main/java/com/axelor/dyn/web/DynController.java
@@ -1,11 +1,13 @@
 package com.axelor.dyn.web;
 
+import java.util.List;
+
 import com.axelor.dyn.db.DynMessage;
 import com.axelor.dyn.service.DynService;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
-import com.google.common.collect.ImmutableMap;
+import com.axelor.rpc.Response;
 import com.google.inject.Inject;
 
 public final class DynController {
@@ -17,8 +19,8 @@ public final class DynController {
 		
 		Context ctx = request.getContext();
 		boolean canReply = "btnReply".equals(ctx.get("_signal"));
-		
-		response.setValues(ImmutableMap.of("canReply", canReply));
+
+		response.setValue("canReply", canReply);
 	}
 
 	public void sendReply(ActionRequest request, ActionResponse response) {
@@ -29,7 +31,32 @@ public final class DynController {
 		reply.setSubject(ctx.getSubject());
 		reply.setBody((String) request.getContext().get("reply"));
 		
-		service.sendMessage(ctx, reply);
-		response.setReload(true);
+		reply = service.sendMessage(ctx, reply);
+
+		// send a view specific signal
+		response.setSignal("trail:record", reply);
+
+		response.setValue("reply", null); 		// clear reply field
+		response.setValue("canReply", false);	// close reply field
+		
+		response.setStatus(ActionResponse.STATUS_SUCCESS);
+	}
+	
+	public void showReplies(ActionRequest request, ActionResponse response) {
+
+		Context ctx = request.getContext();
+
+		response.setSignal("trail:expand", ctx.get("id"));
+		response.setStatus(ActionResponse.STATUS_SUCCESS);
+	}
+
+	public void findAll(ActionRequest request, ActionResponse response) {
+		List<?> all = DynMessage.all().order("createdOn").fetch();
+		
+		response.setData(all);
+		
+		response.setOffset(0);
+		response.setTotal(all.size());
+		response.setStatus(Response.STATUS_SUCCESS);
 	}
 }
