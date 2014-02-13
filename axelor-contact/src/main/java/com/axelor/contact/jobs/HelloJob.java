6ca877e8b9922e373259e79b1b52951bf4d7aff3
Pierre Belloy <p.belloy@axelor.com>
2014-02-14 00:10:37 +0100

    Added a sample scheduled job.

---
+++ axelor-contact/src/main/java/com/axelor/contact/jobs/HelloJob.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Common Public
+ * Attribution License Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://license.axelor.com/.
+ *
+ * The License is based on the Mozilla Public License Version 1.1 but
+ * Sections 14 and 15 have been added to cover use of software over a
+ * computer network and provide for limited attribution for the
+ * Original Developer. In addition, Exhibit A has been modified to be
+ * consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is part of "Axelor Business Suite", developed by
+ * Axelor exclusively.
+ *
+ * The Original Developer is the Initial Developer. The Initial Developer of
+ * the Original Code is Axelor.
+ *
+ * All portions of the code written by Axelor are
+ * Copyright (c) 2012-2014 Axelor. All Rights Reserved.
+ */
+package com.axelor.contact.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * An example {@link Job} class that prints a some messages to the stderr.
+ * 
+ */
+public class HelloJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		JobDetail detail = context.getJobDetail();
+		JobDataMap data = context.getJobDetail().getJobDataMap();
+
+		String name = detail.getKey().getName();
+		String desc = detail.getDescription();
+		
+		System.err.println("Job fired: " + name + " (" + desc + ")");
+		if (data != null && data.size() > 0) {
+			for (String key : data.keySet()) {
+				System.err.println("    " + key + " = " + data.getString(key));
+			}
+		}
+	}
+}
