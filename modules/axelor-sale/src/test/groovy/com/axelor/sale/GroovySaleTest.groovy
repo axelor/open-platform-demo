f76f343fd36f59c141b6b307ac55c0f34050dc35
Pierre Belloy <p.belloy@axelor.com>
2014-09-26 01:09:43 +0200

    Fixed axelor-sale unit tests.

--- modules/axelor-sale/src/test/groovy/com/axelor/sale/GroovySaleTest.groovy
+++ modules/axelor-sale/src/test/groovy/com/axelor/sale/GroovySaleTest.groovy
@@ -17,29 +17,39 @@
  */
 package com.axelor.sale
 
-import java.util.Random
+import groovy.transform.CompileStatic
+
+import javax.inject.Inject
 
 import org.joda.time.LocalDate
+import org.junit.Assert;
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import com.axelor.contact.db.Address
+import com.axelor.contact.db.Circle
 import com.axelor.contact.db.Contact
 import com.axelor.contact.db.Country
-import com.axelor.contact.db.Circle
+import com.axelor.contact.db.Email
 import com.axelor.contact.db.Title
-import com.axelor.db.JPA;
-import com.axelor.db.Model
+import com.axelor.db.JpaSupport
 import com.axelor.sale.db.Order
 import com.axelor.sale.db.OrderLine
 import com.axelor.sale.db.Product
+import com.axelor.sale.db.repo.OrderRepository
 import com.axelor.test.GuiceModules
 import com.axelor.test.GuiceRunner
-import com.google.inject.persist.Transactional;
+import com.google.inject.persist.Transactional
 
 @RunWith(GuiceRunner.class)
 @GuiceModules([ TestModule.class ])
-class GroovySaleTest {
+@CompileStatic
+class GroovySaleTest extends JpaSupport {
+
+	@Inject
+	OrderRepository orders
+
+	final long MAX_COUNT = 100L
 
 	Order createSaleOrder() {
 
@@ -61,10 +71,11 @@ class GroovySaleTest {
 		def contact = new Contact(
 			title: title,
 			firstName: "FirstName_" + next,
-			lastName: "LastName_" + next,
-			email: "first.last.${next}@gmail.com",
-			circle: circle)
-		
+			lastName: "LastName_" + next)
+
+		contact.addEmail(new Email(email: "first.last.${next}@gmail.com"));
+		contact.addCircle(circle);
+
 		def addr1 = new Address(
 			street: "My",
 			area: "Home",
@@ -77,7 +88,8 @@ class GroovySaleTest {
 			city: "Paris",
 			country: country);
 		
-		contact.addresses = [addr1, addr2]
+		contact.addAddress(addr1)
+		contact.addAddress(addr2)
 		
 		def p1 = new Product(
 			code: "pc_" + next,
@@ -104,48 +116,49 @@ class GroovySaleTest {
 			price: new BigDecimal("934.33"),
 			quantity: random.nextInt(20) + 1)
 
-		order.items = [item1, item2]
+		order.addItem(item1)
+		order.addItem(item2)
 		
 		return order
 	}
 
 	@Transactional
 	void createData() {
-		for(i in 0..<1000) {
-			JPA.manage(createSaleOrder())
+		for(i in 0..<MAX_COUNT) {
+			orders.save(createSaleOrder())
 		}
 	}
 
 	@Transactional
 	void dropData() {
-		OrderLine.all().delete();
-		Order.all().delete();
-		Product.all().delete();
-		Address.all().delete();
-		Contact.all().delete();
-		Country.all().delete();
-		Circle.all().delete();
-		Title.all().delete();
+		all(OrderLine).delete();
+		all(Order).delete();
+		all(Product).delete();
+		all(Address).delete();
+		all(Email).delete();
+		all(Contact).delete();
+		all(Country).delete();
+		all(Circle).delete();
+		all(Title).delete();
 	}
 
 	void listData() {
-		System.err.println("Title: " + Title.all().count());
-		System.err.println("Circle: " + Circle.all().count());
-		System.err.println("Country: " + Country.all().count());
-		System.err.println("Contact: " + Contact.all().count());
-		System.err.println("Address: " + Address.all().count());
-		System.err.println("Product: " + Product.all().count());
-		System.err.println("Order: " + Order.all().count());
-		System.err.println("OrderLine: " + OrderLine.all().count());
+		System.err.println("Title: " + all(Title).count());
+		System.err.println("Circle: " + all(Circle).count());
+		System.err.println("Country: " + all(Country).count());
+		System.err.println("Contact: " + all(Contact).count());
+		System.err.println("Address: " + all(Address).count());
+		System.err.println("Product: " + all(Product).count());
+		System.err.println("Order: " + all(Order).count());
+		System.err.println("OrderLine: " + all(OrderLine).count());
 	}
 
 	@Test
 	public void test() {
-		System.err.println("=======|| Create Random Records ||======");
 		createData();
-		listData();
-		System.err.println("=======|| Delete Random Records ||======");
+		Assert.assertEquals(MAX_COUNT, all(Order).count());
+
 		dropData();
-		listData();
+		Assert.assertEquals(0L, all(Order).count());
 	}
 }
