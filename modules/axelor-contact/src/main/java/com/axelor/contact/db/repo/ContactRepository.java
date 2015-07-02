ebb05f992413c99160eb2fcfdc93decaf6a81f28
admin <admin@axelor.com>
2015-07-02 13:31:18 +0200

    Add cards view demo

---
+++ modules/axelor-contact/src/main/java/com/axelor/contact/db/repo/ContactRepository.java
@@ -0,0 +1,41 @@
+/**
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2015 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.contact.db.repo;
+
+import java.util.Map;
+
+import com.axelor.contact.db.Contact;
+
+public class ContactRepository extends AbstractContactRepository {
+
+	@Override
+	public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
+		if (!context.containsKey("json-enhance")) {
+			return json;
+		}
+		try {
+			Long id = (Long) json.get("id");
+			Contact contact = find(id);
+			json.put("address", contact.getAddresses().get(0));
+			json.put("hasImage", contact.getImage() != null);
+		} catch (Exception e) {
+		}
+
+		return json;
+	}
+}
