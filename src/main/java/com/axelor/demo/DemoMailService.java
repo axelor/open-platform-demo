f734e8afeffc3924e0a71acdee0d26bbf833cfa1
admin <admin@axelor.com>
2015-10-23 00:38:49 +0200

    Override MailService.resolve method
    Demonstrates how to resolve an email address to it's
    associated entities.

--- src/main/java/com/axelor/demo/DemoMailService.java
+++ src/main/java/com/axelor/demo/DemoMailService.java
@@ -29,12 +29,25 @@ import javax.inject.Singleton;
 import javax.mail.internet.InternetAddress;
 
 import com.axelor.contact.db.Contact;
+import com.axelor.contact.db.repo.ContactRepository;
+import com.axelor.db.Model;
 import com.axelor.db.Query;
+import com.axelor.inject.Beans;
 import com.axelor.mail.service.MailServiceImpl;
 import com.google.common.base.Joiner;
 
 @Singleton
 public class DemoMailService extends MailServiceImpl {
+
+	@Override
+	public Model resolve(String email) {
+		final ContactRepository contacts = Beans.get(ContactRepository.class);
+		final Contact contact = contacts.findByEmail(email);
+		if (contact != null) {
+			return contact;
+		}
+		return super.resolve(email);
+	}
 
 	/**
 	 * Override to return contact email addresses.
