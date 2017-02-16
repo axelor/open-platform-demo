fac5d61a0381486b50ee4e2ce607e3066143cca3
admin <admin@axelor.com>
2017-02-16 15:22:13 +0100

    Add unit test to check data import

---
+++ src/test/java/com/axelor/app/DataTest.java
@@ -0,0 +1,40 @@
+/**
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2017 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.app;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.axelor.data.xml.XMLImporter;
+import com.axelor.test.GuiceModules;
+import com.axelor.test.GuiceRunner;
+
+@RunWith(GuiceRunner.class)
+@GuiceModules({ MyModule.class })
+public class DataTest {
+
+	@Test
+	public void test() throws FileNotFoundException {
+		XMLImporter importer = new XMLImporter(
+				"src/main/resources/data-demo/input-config.xml",
+				"src/main/resources/data-demo/input");
+		importer.run();
+	}
+}
