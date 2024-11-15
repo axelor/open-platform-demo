c335fce93146b2c46800af6ce2da803d9368f7fe
Pierre Belloy <p.belloy@axelor.com>
2024-11-15 10:11:35 +0100

    Add totalSales InfoButton on product form view
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

---
+++ modules/demo-sale/src/main/java/com/axelor/sale/web/ProductController.java
@@ -0,0 +1,52 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.axelor.sale.web;
+
+import com.axelor.db.JPA;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ProductController {
+
+  public void fetchTotalSales(ActionRequest request, ActionResponse response) {
+    Long productId;
+    try {
+      productId = Long.valueOf(request.getContext().get("id").toString());
+    } catch (Exception e) {
+      return;
+    }
+
+    BigDecimal sales =
+        JPA.em()
+            .createQuery(
+                "SELECT sum(self.quantity * self.price) "
+                    + "FROM OrderLine self "
+                    + "LEFT JOIN self.order as o "
+                    + "LEFT JOIN self.product as p "
+                    + "WHERE p.id = :productId AND o.confirmed IS TRUE AND YEAR(o.orderDate) = :year",
+                BigDecimal.class)
+            .setParameter("productId", productId)
+            .setParameter("year", LocalDate.now().getYear())
+            .getSingleResult();
+
+    response.setValue("$btnTotalSales", sales == null ? BigDecimal.ZERO : sales);
+  }
+}
