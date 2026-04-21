95cef1034578fec56c6737e69ac7e3b8efad6aa9
Pierre Belloy <p.belloy@axelor.com>
2026-04-21 13:09:23 +0200

    Avoid fetching full Contact entity in repository
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- modules/demo-contact/src/main/java/com/axelor/contact/db/repo/ContactRepository.java
+++ modules/demo-contact/src/main/java/com/axelor/contact/db/repo/ContactRepository.java
@@ -4,8 +4,8 @@
  */
 package com.axelor.contact.db.repo;
 
-import com.axelor.common.ObjectUtils;
-import com.axelor.contact.db.Contact;
+import com.axelor.db.JPA;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ContactRepository extends AbstractContactRepository {
@@ -17,12 +17,38 @@ public class ContactRepository extends AbstractContactRepository {
     }
     try {
       Long id = (Long) json.get("id");
-      Contact contact = find(id);
-      json.put(
-          "address",
-          ObjectUtils.isEmpty(contact.getAddresses()) ? null : contact.getAddresses().get(0));
-      json.put("hasImage", contact.getImage() != null);
+      var result =
+          JPA.em()
+              .createQuery(
+                  """
+                              SELECT CASE WHEN self.image IS NOT NULL THEN TRUE ELSE FALSE END as hasImage,
+                              a.street as street, a.area as area, a.city as city, a.state as state, a.zip as zip, country.name as country
+                              FROM Contact self
+                              LEFT JOIN Address a ON a.contact = self AND a.id = (SELECT MIN(a2.id) FROM Address a2 WHERE a2.contact = self)
+                              LEFT JOIN a.country country
+                              WHERE self.id = :cId
+                              """,
+                  Object[].class)
+              .setParameter("cId", id)
+              .getResultStream()
+              .findFirst()
+              .orElse(null);
+      if (result == null) {
+        return json;
+      }
+      json.put("hasImage", result[0]);
+
+      Map<Object, Object> map = new HashMap<>();
+      map.put("street", result[1]);
+      map.put("area", result[2]);
+      map.put("city", result[3]);
+      map.put("state", result[4]);
+      map.put("zip", result[5]);
+      map.put("country", result[6]);
+
+      json.put("address", map);
     } catch (Exception e) {
+      // ignore
     }
 
     return json;
