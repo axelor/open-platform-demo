ec955130f3534a0a90f6d7b9f311f8f6c5606489
Pierre Belloy <p.belloy@axelor.com>
2026-05-06 09:50:25 +0200

    Add custom Vehicle json models and fields
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- src/main/java/com/axelor/demo/CustomModelImport.java
+++ src/main/java/com/axelor/demo/CustomModelImport.java
@@ -5,21 +5,22 @@
 package com.axelor.demo;
 
 import com.axelor.inject.Beans;
+import com.axelor.meta.MetaFiles;
+import com.axelor.meta.db.MetaFile;
 import com.axelor.meta.db.MetaJsonModel;
 import com.axelor.meta.db.MetaJsonRecord;
 import com.axelor.meta.db.repo.MetaJsonModelRepository;
 import com.axelor.meta.db.repo.MetaJsonRecordRepository;
 import com.axelor.rpc.Context;
+import java.nio.file.Path;
 import java.util.Map;
 
 public class CustomModelImport {
 
+  private static final String VEHICLE_IMAGES_DIR = "vehicle_images";
+
   public Object importCustomModel(Object bean, Map context) {
-    MetaJsonModel model = (MetaJsonModel) bean;
-
-    Beans.get(MetaJsonModelRepository.class).save(model);
-
-    return model;
+    return Beans.get(MetaJsonModelRepository.class).save((MetaJsonModel) bean);
   }
 
   public Object importProductExtraOptions(Object bean, Map context) {
@@ -31,5 +32,31 @@ public class CustomModelImport {
     extraOptions.put("importId", context.get("importId"));
 
     return repo.save(record);
+  }
+
+  public Object importVehicle(Object bean, Map context) {
+
+    MetaJsonRecord record = (MetaJsonRecord) bean;
+    MetaJsonRecordRepository repo = Beans.get(MetaJsonRecordRepository.class);
+    Context vehicle = repo.create(record);
+
+    try {
+      String fileName = context.get("name").toString().replace(" ", "-").toLowerCase();
+      final Path image =
+          ImportUtils.findByFileName(
+              ((Path) context.get("__path__")).resolve(VEHICLE_IMAGES_DIR), fileName);
+      if (image != null && image.toFile().exists()) {
+        final MetaFile metaFile = Beans.get(MetaFiles.class).upload(image.toFile());
+        vehicle.put("image", metaFile);
+      }
+    } catch (Exception e) {
+      // ignore
+    }
+
+    return repo.save(record);
+  }
+
+  public Object saveMetaJsonRecord(Object bean, Map context) {
+    return Beans.get(MetaJsonRecordRepository.class).save((MetaJsonRecord) bean);
   }
 }
